f3e21cccc85d620806b3354e7fcc1f40e46fc3be
Albert Cordina <137774947+albertcordina@users.noreply.github.com>
2023-11-12 19:18:44 +0100

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,1 +1,30 @@
+public class Vehicle {
+	
+	String typeOfVehicle;
+	double timeFrom;
+	double timeTo;
+	
+	public Vehicle (String typeOfVehicle, double timeFrom, double timeTo) {
+		
+		this.typeOfVehicle = typeOfVehicle;
+		this.timeFrom = timeFrom;
+		this.timeTo = timeTo;
+	}
 
+	public String getTypeOfVehicle() {
+		return typeOfVehicle;
+	}
+
+	public double getTimeFrom() {
+		return timeFrom;
+	}
+
+	public double getTimeTo() {
+		return timeTo;
+	}
+
+	@Override
+	public String toString() {
+		return "The type of the vehicle: " + typeOfVehicle + ", the allowed driving period from " + timeFrom + " to " + timeTo;
+	}
+}
